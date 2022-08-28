9408f6722831305b85879c4f18b867770b2aab29
Shubham Kumar <73250851+shubhamdev-code@users.noreply.github.com>
2022-08-28 18:16:56 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,15 +41,15 @@ void findMinAndMax(vector<int> const &nums, int low, int high, int &min, int &ma
  findMinAndMax(nums, mid + 1, high, min, max);
 }
 int main(){
- vector<int> nums = { 7, 2, 9, 3, 1, 6, 7, 8, 4 };
- 
- int max = INT_MIN, min = INT_MAX;
- 
- int n = nums.size();
- findMinAndMax(nums, 0, n - 1, min, max);
- 
- cout << "The minimum array element is " << min << endl;
- cout << "The maximum array element is " << max << endl;
- 
- return 0;
+  vector<int> nums = { 7, 2, 9, 3, 1, 6, 7, 8, 4 };
+
+  int max = INT_MIN, min = INT_MAX;
+
+  int n = nums.size();
+  findMinAndMax(nums, 0, n - 1, min, max);
+
+  cout << "The minimum array element is " << min << endl;
+  cout << "The maximum array element is " << max << endl;
+
+  return 0;
 }
